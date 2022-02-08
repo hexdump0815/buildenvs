4590e3190a74e894da89edba97d8bc969db27069
hexdump <hexdump0815@googlemail.com>
2022-02-08 19:03:37 +0100

    64bit raspbian is simply debian bullseye

--- docker-buildenv-raspbian-bullseye.sh
+++ docker-buildenv-raspbian-bullseye.sh
@@ -8,11 +8,9 @@ export DEBIAN_FRONTEND=noninteractive && apt-get -yq install debootstrap
 
 if [ "$(uname -m)" = "armv7l" ]; then
   BOOTSTRAP_ARCH="armhf"
-elif [ "$(uname -m)" = "aarch64" ]; then
-  BOOTSTRAP_ARCH="arm64"
 else
   echo ""
-  echo "arch is $(uname -m) - only armv7l and aarch64 are supported"
+  echo "arch is $(uname -m) - only armv7l is supported"
   echo ""
   exit 1
 fi
